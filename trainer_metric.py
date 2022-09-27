e7e9ff08095797e1a75e349361dff88d24f8ad9f
gu <dlfrnaos19@gmail.com>
2022-09-27 23:00:26 +0900

    remove compute_metric, metric update, readme

--- trainer_metric.py
+++ trainer_metric.py
@@ -135,30 +135,3 @@ def compute_bleu(decoded_preds, decoded_labels):
     result = bleu_metric.compute(predictions=decoded_preds, references=decoded_labels)
     
     return {"bleu": result["score"]}
-
-def compute_metrics(eval_preds):
-        
-    preds, labels = eval_preds.predictions, eval_preds.label_ids
-    # In case the model returns more than the prediction logits
-    if isinstance(preds, tuple):
-        preds = preds[0]
-
-    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
-
-    # Replace -100s in the labels as we can't decode them
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-
-    # Some simple post-processing
-    decoded_preds = [pred.strip() for pred in decoded_preds]
-    bleu_decoded_labels = [[label.strip()] for label in decoded_labels]
-    decoded_labels = [label.strip() for label in decoded_labels]
-    
-    metric_dict = {}
-    bleu_dict = compute_bleu(decoded_preds=decoded_preds, decoded_labels=bleu_decoded_labels)
-    f1_acc_dict = compute_f1_acc(decoded_preds=decoded_preds, decoded_labels=decoded_labels)
-    
-    metric_dict.update(bleu_dict)
-    metric_dict.update(f1_acc_dict)
-    
-    return metric_dict
